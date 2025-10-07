9cdf9ad8483ba2eb5966afd5ecb49398c75bf9bb
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-10-07 18:56:57 +1100

    Update scraper

--- scraper.py
+++ scraper.py
@@ -1,21 +1,25 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import lxml.html
+
+# Read in a page
+html = scraperwiki.scrape("https://example.com")
+
+# Find something on the page using css selectors
+root = lxml.html.fromstring(html)
+h1s = root.cssselect("h1")
+
+# Write out to the sqlite database using scraperwiki library
+for h1 in h1s:
+    value = h1.text_content().strip()
+    scraperwiki.sqlite.save(unique_keys=['name'], data={"name": value})
+
+# An arbitrary query against the database
+if h1s:
+    rows = scraperwiki.sql.select("rowid AS id, name FROM data ORDER BY rowid desc LIMIT 3")
+    for row in rows:
+        print(f"{row['id']}: {row['name']}")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
